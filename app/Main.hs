2e5100102e2d99f9b40b0d1efbf7d66a649cb48d
Veronika Romashkina <vrom911@gmail.com>
2020-05-23 22:38:01 +0100

    Introduce TaggedTrial (#19)

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Data.Text (Text)
 import Options.Applicative (Parser, ReadM, auto, execParser, info, long, option, optional, str)
 import Toml (Key, TomlCodec, (.=))
 
-import Trial (Fatality (..), Trial (..), maybeToTrial, trialToMaybe, withTag)
+import Trial (Fatality (..), TaggedTrial, Trial (..), maybeToTrial, trialToMaybe, withTag)
 
 import qualified Data.Text as T
 import qualified Toml
@@ -27,9 +27,9 @@ data Options = Options
     } deriving stock (Show, Eq)
 
 data PartialOptions = PartialOptions
-    { poRetryCount    :: !(Trial Text (Text, Int))
-    , poHost          :: !(Trial Text (Text, Text))
-    , poCharacterCode :: !(Trial Text (Text, Maybe Bool))
+    { poRetryCount    :: !(TaggedTrial Text Int)
+    , poHost          :: !(TaggedTrial Text Text)
+    , poCharacterCode :: !(TaggedTrial Text (Maybe Bool))
     } deriving stock (Show, Eq)
 
 instance Semigroup PartialOptions where
@@ -53,10 +53,10 @@ defaultPartialOptions = PartialOptions
     , poCharacterCode = withTag "Default" $ Fiasco []
     }
 
-mToTrial :: Text -> Text -> Maybe a -> Trial Text (Text, a)
+mToTrial :: Text -> Text -> Maybe a -> TaggedTrial Text a
 mToTrial from field = withTag from . maybeToTrial ("No " <> from <> " option specified for " <> field)
 
-trialOption :: Text -> ReadM a -> Parser (Trial Text (Text, a))
+trialOption :: Text -> ReadM a -> Parser (TaggedTrial Text a)
 trialOption field opt = mToTrial "CLI" field <$>
     optional (option opt (long $ T.unpack field))
 
@@ -74,16 +74,16 @@ partialOptionsCodec = PartialOptions
     <*> trialCodec "host"        Toml.text .= poHost
     <*> trialMaybeCodec "character-code"   .= poCharacterCode
   where
-    trialMaybeCodec :: Key -> TomlCodec (Trial Text (Text, Maybe Bool))
+    trialMaybeCodec :: Key -> TomlCodec (TaggedTrial Text (Maybe Bool))
     trialMaybeCodec key = Toml.dimap f (withTag "TOML" . pure) $
         Toml.dioptional (Toml.bool key)
 
-    f :: Trial Text (Text, Maybe a) -> Maybe a
+    f :: TaggedTrial Text (Maybe a) -> Maybe a
     f = \case
         Result _ a -> snd a
         Fiasco _ -> Nothing
 
-trialCodec :: Key -> (Key -> TomlCodec a) -> TomlCodec (Trial Text (Text, a))
+trialCodec :: Key -> (Key -> TomlCodec a) -> TomlCodec (TaggedTrial Text a)
 trialCodec key codecA = Toml.dimap (fmap snd . trialToMaybe) (mToTrial "TOML" $ Toml.prettyKey key) $
     Toml.dioptional (codecA key)
 
